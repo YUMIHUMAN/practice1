69c901637b71099ad00f142e572c9c2cc898b565
KH <h.kikuchi.sys23@morijyobi.ac.jp>
2023-10-05 10:53:01 +0900

    Hello world
    100回繰り返すプログラムに変更しました

--- pra2.java
+++ pra2.java
@@ -1,5 +1,7 @@
-public class pra2{
+ipublic class pra2{
     public static void Main(String[]args){
+     for(int n = 0; n < 100; n++){	    
         System.out.println("Hello World");
     }
+  }
 }
